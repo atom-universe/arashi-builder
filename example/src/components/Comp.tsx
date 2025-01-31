a70ef1bb3da4614df74099dd1c74911c8beeb66d
Carbon 碳苯 <2779066456@qq.com>
2025-01-31 15:41:51 +0800

    feat: Add a React Example

---
+++ example/src/components/Comp.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+const Comp = () => {
+  return (
+    <div className="container">
+      <h1>Arashi</h1>
+      <img src="pic.png" alt="pic!" />
+    </div>
+  );
+};
+
+export default Comp;
